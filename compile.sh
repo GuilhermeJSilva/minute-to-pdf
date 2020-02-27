30865ab70a854730c5016726a1f25944c9d61be4
Guilherme Silva <guilhermejosesilva@gmail.com>
2020-02-27 15:20:59 +0000

    Add script args validation and default args

--- compile.sh
+++ compile.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-FILENAME=$1
+set -e
 
-python "$DIR/src/main.py" "$FILENAME" | pandoc -o $2
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [[ -z $1 ]] ; then
+    echo "Usage: $0 <input-file> [ <output-file> ]"
+    exit -1
+fi
+
+FILENAME=$1
+OUTFILE=${2:-"${1%.*}.pdf"}
+
+python "$DIR/src/main.py" "$FILENAME" | pandoc -o "$OUTFILE"
