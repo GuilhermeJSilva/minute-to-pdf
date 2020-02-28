a46bef9cc76318c702f8d94da4ad6186bcdf0e3b
Guilherme Silva <guilhermejosesilva@gmail.com>
2020-02-28 17:18:15 +0000

    Add option to download form hackMD

--- compile.sh
+++ compile.sh
@@ -3,12 +3,43 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-if [[ -z $1 ]] ; then
-    echo "Usage: $0 <input-file> [ <output-file> ]"
-    exit -1
+FILENAME=""
+DOWNLOAD_URL=""
+OUTFILE=""
+
+while [[ "$#" -gt 0 ]]
+do
+    case $1 in
+        --url)
+            DOWNLOAD_URL="$2"
+            shift
+        ;;
+        --out)
+            OUTFILE="$2"
+            shift
+        ;;
+        *)
+            FILENAME="$1"
+        ;;
+    esac
+    shift
+done
+
+if [[ "$DOWNLOAD_URL" == "" ]]; then
+    OUTFILE=${OUTFILE:-"${FILENAME%.*}.pdf"}
 fi
 
-FILENAME=$1
-OUTFILE=${2:-"${1%.*}.pdf"}
+echo "FILENAME: $FILENAME"
+echo "DOWNLOAD_URL: $DOWNLOAD_URL"
+echo "OUTFILE: $OUTFILE"
 
-python3 "$DIR/src/main.py" "$FILENAME" | pandoc -o "$OUTFILE"
+if [[ "$DOWNLOAD_URL" == "" ]]; then
+    python3 "$DIR/src/main.py" "$FILENAME" | pandoc -o "$OUTFILE"
+else
+    if [[ "$OUTFILE" == "" ]]; then
+        echo "Missing the output file. Use the option --out <filename> to specify one."
+        exit -1
+    fi
+
+    curl "$DOWNLOAD_URL/download" | python3 "$DIR/src/main.py" | pandoc -o "$OUTFILE"
+fi
--- src/main.py
+++ src/main.py
@@ -3,21 +3,21 @@ import uuid
 import cleanMarkdown
 from convGraph import replaceGraphViz
 
+def buildSource(sourceCode, operationUuid):
+    sourceCode = replaceGraphViz(sourceCode, operationUuid=operationUuid)
+    sourceCode = cleanMarkdown.removeTags(sourceCode)
+    title, sourceCode = cleanMarkdown.getTitle(sourceCode)
+    sourceCode = cleanMarkdown.downsizeTitles(sourceCode)
+    sourceCode = cleanMarkdown.spacePoints(sourceCode)
+    return cleanMarkdown.addFrontmatter(sourceCode, title)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print(
-            f'Missing source file.\nUsage: python {sys.argv[0]} <source-file>')
-        sys.exit(-1)
-
     operationUuid = uuid.uuid1().hex
-    filename = sys.argv[1]
-    with open(filename, 'r') as file:
-        sourceCode = file.read()
-        sourceCode = replaceGraphViz(sourceCode, operationUuid=operationUuid)
-        sourceCode = cleanMarkdown.removeTags(sourceCode)
-        title, sourceCode = cleanMarkdown.getTitle(sourceCode)
-        sourceCode = cleanMarkdown.downsizeTitles(sourceCode)
-        sourceCode = cleanMarkdown.spacePoints(sourceCode)
-        sourceCode = cleanMarkdown.addFrontmatter(sourceCode, title)
-        print(sourceCode)
+    if len(sys.argv) < 2:
+        sourceCode = sys.stdin.read()
+        print(buildSource(sourceCode, operationUuid))
+    else:
+        filename = sys.argv[1]
+        with open(filename, 'r') as file:
+            sourceCode = file.read()
+            print(buildSource(sourceCode, operationUuid))
