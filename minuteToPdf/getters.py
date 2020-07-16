6621b0c47f3ce8f27f7d4247dbf02b34484cb842
Guilherme Silva <guilhermejosesilva@gmail.com>
2020-07-16 20:14:24 +0100

    Add output file deduction

--- minuteToPdf/getters.py
+++ minuteToPdf/getters.py
@@ -3,7 +3,10 @@ import re as regex
 
 def getTitle(sourceCode):
     titleRegex = r"^# ([^\n]*)"
-    return regex.search(titleRegex, sourceCode).group(1)
+    finding = regex.search(titleRegex, sourceCode)
+    if finding is not None:
+        return finding.group(1)
+    return ""
 
 
 def retrieveCommand(sourceCode, command):
